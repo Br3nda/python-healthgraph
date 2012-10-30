a447dc283ed899b04d3a65045012499a6223cecb
Brenda Wallace <shiny@cpan.org>
2012-10-30 16:54:40 +1300

    Unit tests

---
+++ tests/test_pylib_runkeeper.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestUser(unittest.TestCase):
+    def test_auth(self):
+        # user = User()
+        # self.assertEqual(expected, user.auth(code))
+        assert False # TODO: implement your test here
+
+    def test_auth_url(self):
+        # user = User()
+        # self.assertEqual(expected, user.auth_url())
+        assert False # TODO: implement your test here
+
+    def test_save_auth_code(self):
+        # user = User()
+        # self.assertEqual(expected, user.save_auth_code(code))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
