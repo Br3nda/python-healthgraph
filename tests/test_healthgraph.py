9dc4dcffea9a5da8fff2b35440838f36aa43dccc
Brenda Wallace <shiny@cpan.org>
2012-11-08 21:21:03 +1300

    renamed tests

--- tests/test_healthgraph.py
+++ tests/test_healthgraph.py
@@ -11,9 +11,19 @@ class TestUser(unittest.TestCase):
         # self.assertEqual(expected, user.auth_url())
         assert False # TODO: implement your test here
 
-    def test_save_auth_code(self):
+    def test_get_user_details(self):
         # user = User()
-        # self.assertEqual(expected, user.save_auth_code(code))
+        # self.assertEqual(expected, user.get_user_details())
+        assert False # TODO: implement your test here
+
+    def test_save_access_token(self):
+        # user = User()
+        # self.assertEqual(expected, user.save_access_token(token))
+        assert False # TODO: implement your test here
+
+    def test_token(self):
+        # user = User()
+        # self.assertEqual(expected, user.token())
         assert False # TODO: implement your test here
 
 if __name__ == '__main__':
