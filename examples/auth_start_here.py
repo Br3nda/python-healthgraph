51070680790a42db561c07a2ee5d84f261a3ce0a
Brenda Wallace <shiny@cpan.org>
2012-11-14 21:03:39 +1300

    renamed auth

---
+++ examples/auth_start_here.py
@@ -0,0 +1,9 @@
+from healthgraph import auth_url
+print "To begin auth, go to:"
+print auth_url()
+
+print 
+print "Then read code from the url and use auth_save_token.py <code> to save"
+print
+
+
--- examples/auth_step_one.py
+++
@@ -1,4 +0,0 @@
-from healthgraph import auth_url
-print "To begin auth, go to:"
-print auth_url()
-
